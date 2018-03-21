67b0a69c33096ebc56f9ae21a3250d3721fb96bd
GABRIEL GONCALVES MACEDO <gabriel.gmacedo@senacsp.edu.br>
2018-03-21 12:06:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/gabrielgmacedo/travelapp/Editp_Activity.java
@@ -0,0 +1,13 @@
+package com.example.gabrielgmacedo.travelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Editp_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editp_layout);
+    }
+}
---
+++ app/src/main/java/com/example/gabrielgmacedo/travelapp/SignUp_Activity.java
@@ -0,0 +1,13 @@
+package com.example.gabrielgmacedo.travelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_layout);
+    }
+}
